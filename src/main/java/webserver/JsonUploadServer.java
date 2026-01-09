dae502347ddf7a332ab30c6c996bf85f58325c0b
uniquepixel <joni.dormann@gmail.com>
2026-01-09 01:16:46 +0100

    Update JsonUploadServer.java

--- src/main/java/webserver/JsonUploadServer.java
+++ src/main/java/webserver/JsonUploadServer.java
@@ -6,7 +6,6 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.UUID;
